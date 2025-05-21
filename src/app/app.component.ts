5566c649687bd02ea697dc9327c6430be3ceb3ad
Sebastian <sard1996@hotmail.com>
2025-05-21 15:39:26 -0500

    Landing page [incomplete]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { MoreinfoComponent } from './components/moreinfo/moreinfo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutmeComponent, MoreinfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
